bb21aeedd12a34674c7c2cd40d361da60089ee23
Anashkin Aleksei <109337222+NewalexOA@users.noreply.github.com>
2024-05-26 19:31:08 +0300

    disabled logging

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def run_spider():
                 "overwrite": True
             },
         },
-        "USER_AGENT": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        "USER_AGENT": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        "LOG_ENABLED": False
     })
 
     process.crawl(DivanSpider)
