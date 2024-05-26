0cb1788b82c320e5273c7e8223b7ceec751743bc
Anashkin Aleksei <109337222+NewalexOA@users.noreply.github.com>
2024-05-26 19:01:26 +0300

    update

--- divan_scraper/divan_scraper/spiders/divan.py
+++ divan_scraper/divan_scraper/spiders/divan.py
@@ -1,5 +1,4 @@
 import scrapy
-import logging
 
 class DivanSpider(scrapy.Spider):
     name = 'divan'
@@ -10,20 +9,16 @@ class DivanSpider(scrapy.Spider):
 
     def parse(self, response):
         if response.status == 404:
-            self.log('No more pages to load, stopping the spider.', level=scrapy.log.INFO)
             return
-
-        self.log(f'Processing page: {response.url}')
 
         price_containers = response.css('div.q5Uds.T7z9Z.fxA6s')
         for container in price_containers:
             price = container.css('span[data-testid="price"]::text').get()
             if price:
                 price = price.strip().replace(' ', '')
-                self.log(f'Found price: {price}')
                 yield {'Цена': price}
 
-        # Переход на следующую страницу без проверки ссылки
+        # Переход на следующую страницу
         current_page_number = int(response.url.split('page-')[-1].split('?')[0]) if 'page-' in response.url else 1
         next_page_number = current_page_number + 1
         next_page_url = f'https://www.divan.ru/category/divany-i-kresla/page-{next_page_number}?types%5B%5D=1&types%5B%5D=4&types%5B%5D=54'
--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from scrapy.crawler import CrawlerProcess
-from divan_scraper.divan_scraper.spiders.divan import DivanSpider   # Импортируем паука
+from divan_scraper.divan_scraper.spiders.divan import DivanSpider
 
 def run_spider():
     process = CrawlerProcess(settings={
         "FEEDS": {
-            "divan_prices.csv": {"format": "csv"},
+            "divan_prices.csv": {
+                "format": "csv",
+                "overwrite": True
+            },
         },
         "USER_AGENT": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     })
 
     process.crawl(DivanSpider)
-    process.start()  # Блокирует выполнение, пока паук не завершит работу
+    process.start()
 
 if __name__ == "__main__":
     run_spider()
 
-df = pd.read_csv('divan_prices.csv')
+    df = pd.read_csv('divan_prices.csv')
+    print(f'Средняя цена: {int(df.mean().item())} рублей.')
 
-print(f'Средняя цена: {int(df.mean().item())} рублей.')
-
-plt.figure(figsize=(10, 5))
-plt.title('Распределение цен')
-plt.xlabel('Цена')
-plt.ylabel('Количество')
-plt.grid(True)
-plt.hist(df)
-plt.show()
+    plt.figure(figsize=(10, 5))
+    plt.title('Распределение цен')
+    plt.xlabel('Цена')
+    plt.ylabel('Количество')
+    plt.grid(True)
+    plt.hist(df['Цена'], bins=20)
+    plt.show()
